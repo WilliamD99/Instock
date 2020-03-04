8a2278b740ccf97ffd432a7561820559bd1aeb5a
WilliamD99 <dnam310199@gmail.com>
2020-03-04 15:44:26 -0800

    Modal finished

--- app/src/App.js
+++ app/src/App.js
@@ -2,24 +2,13 @@ import React from "react";
 import "./styles/main.css";
 import Location from "./components/Location";
 import Modal from "./components/Modal";
-import $ from "jquery";
 
 function App() {
-  function revealModal() {
-    let modal = $("#modal");
-    modal.fadeIn("slow", () => {
-      modal.css({ display: "flex", "flex-direction": "column" });
-    });
-  }
   return (
     <div className="App">
-      <button className="test-button" onClick={revealModal}>
-        Click me
-      </button>
-      <Modal></Modal>
+      <Modal />
       <Location />
     </div>
   );
 }
-
 export default App;
