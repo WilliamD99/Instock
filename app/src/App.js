5b513ebcd9629204a81a8fcf68180886da33a6bb
Vinicius Manrique <viniciusmanrique@gmail.com>
2020-03-09 01:31:30 -0700

    Finished the Inventory component and added the Product component, with some minor adjustments to be done and integration with serve side

--- app/src/App.js
+++ app/src/App.js
@@ -4,14 +4,16 @@ import Location from "./components/Location";
 import Modal from "./components/Modal";
 import Header from "./components/Header";
 import Inventory from "./components/Inventory";
+import Product from "./components/Product";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Modal />
-      <Location />
+      <Product />
       <Inventory />
+      <Location />
     </div>
   );
 }
--- app/src/components/Inventory.js
+++ app/src/components/Inventory.js
@@ -13,10 +13,10 @@ export default function Inventory() {
           date: "1549152000",
           location: {
             city: "Toronto",
-            country: "Canada"
+            country: "CAN"
           },
           quantity: "12000",
-          status: "1",
+          status: "In Stock",
           categories: [
             "Industrial",
             "Automotive",
@@ -40,10 +40,10 @@ export default function Inventory() {
           date: "1549152000",
           location: {
             city: "Toronto",
-            country: "Canada"
+            country: "CAN"
           },
           quantity: "12000",
-          status: "1",
+          status: "In Stock",
           categories: [
             "Industrial",
             "Automotive",
@@ -67,10 +67,10 @@ export default function Inventory() {
           date: "1549152000",
           location: {
             city: "Toronto",
-            country: "Canada"
+            country: "CAN"
           },
           quantity: "12000",
-          status: "1",
+          status: "In Stock",
           categories: [
             "Industrial",
             "Automotive",
@@ -95,33 +95,33 @@ export default function Inventory() {
         //map through categories here
         return (
           <div className="inventory__item">
-            <div className="inventory__warehouse-wrap">
-              <h5 className="inventory__label">Item</h5>
-              <div className="inventory__warehouse-button-wrap">
-                <h2 className="inventory__name">{objectsList.name}</h2>
+            <div className="inventory__product-item-wrap">
+              <div className="inventory__item-button-wrap">
+                <h5 className="inventory__label">Item</h5>
                 <button className="inventory__button">></button>
               </div>
-              <h2 className="inventory__info">
-                {/* {listData.address.street} */}
+              <h2 className="inventory__name">{objectsList.name}</h2>
+              <h2 className="inventory__description">
+                {objectsList.description}
               </h2>
             </div>
-            <div className="inventory__contact-wrap">
+            <div className="inventory__order-wrap">
               <h5 className="inventory__label">Last Ordered</h5>
-              <h2 className="inventory__order">{/* {listData.date} */}</h2>
-              <h2 className="inventory__contact-title">
-                {/* {listData.quantity} */}
+              <h2 className="inventory__date">{objectsList.date}</h2>
+            </div>
+            <div className="inventory__location-wrap">
+              <h5 className="inventory__label">Location</h5>
+              <h2 className="inventory__location">
+                {objectsList.location.city}, {objectsList.location.country}
               </h2>
             </div>
-            <div className="inventory__information-wrap">
-              <h5 className="inventory__label">Contact Information</h5>
-              <h2 className="inventory__info">{/* {listData.status} */}</h2>
-              <h2 className="inventory__info">
-                {/* {listData.contact.email} */}
-              </h2>
+            <div className="inventory__quantity-wrap">
+              <h5 className="inventory__label">Quantity</h5>
+              <h2 className="inventory__quantity">{objectsList.quantity}</h2>
             </div>
-            <div className="inventory__categories-wrap">
-              <h5 className="inventory__label">Categories</h5>
-              <h2 className="inventory__info">{/* {listOfCategories} */}</h2>
+            <div className="inventory__status-wrap">
+              <h5 className="inventory__label">Status</h5>
+              <h2 className="inventory__status">{objectsList.status}</h2>
             </div>
           </div>
         );
@@ -137,7 +137,7 @@ export default function Inventory() {
   });
 
   return (
-    <section className="location">
+    <section className="inventory">
       <div className="inventory__top-wrap">
         <h1 className="inventory__section-title">Inventory</h1>
         <form className="inventory__form" id="form">
