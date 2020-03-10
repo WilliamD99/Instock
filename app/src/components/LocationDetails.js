4d43e9473f9bb5af20f9ba7ff7f209ef5a7d85e5
Vinicius Manrique <viniciusmanrique@gmail.com>
2020-03-09 17:54:28 -0700

    Edit the Location component, still need to make some button adjustments in another components

--- app/src/components/LocationDetails.js
+++ app/src/components/LocationDetails.js
@@ -1,56 +1,100 @@
 import React from "react";
 import Arrow from "../assets/Icons/SVG/Icon-back-arrow.svg";
-import { Link } from "react-router-dom";
+import { Link, useRouteMatch } from "react-router-dom";
+import checkStatus from "../helpers/checkStatus";
 
-export default function LocationDetails({ locations }) {
+export default function LocationDetails({ location, items }) {
   let contents = [];
-  if (locations !== null) {
-    locations.map((location, index) => {
-      let main = (
-        <div className="location-details__item-wrap" key={index}>
-          <div className="location-details__title-wrap">
-            <Link to="/location">
-              <img className="location-details__icon" src={Arrow} alt="Arrow" />
-            </Link>
-            <h1 className="location-details__section-title">{location.name}</h1>
+  console.log(items);
+  let { url } = useRouteMatch();
+  let main = (
+    <div className="location-details__detail-location-wrap">
+      <div className="location-details__item-wrap">
+        <div className="location-details__title-wrap">
+          <Link to="/location">
+            <img className="location-details__icon" src={Arrow} alt="Arrow" />
+          </Link>
+          <h1 className="location-details__section-title">{location.name}</h1>
+        </div>
+        <div className="location-details__item">
+          <div className="location-details__address-wrap">
+            <h5 className="location-details__label">Address</h5>
+            <h2 className="location-details__info">
+              {location.address.street}
+            </h2>
+            <h2 className="location-details__info-gap">
+              {location.address.number}
+            </h2>
+            <h2 className="location-details__info">
+              {location.address.city}, {location.address.province}
+            </h2>
+            <h2 className="location-details__info">
+              {location.address.postal}, {location.address.country}
+            </h2>
           </div>
-          <div className="location-details__item">
-            <div className="location-details__address-wrap">
-              <h5 className="location-details__label">Address</h5>
-              <h2 className="location-details__info">
-                {location.address.street}
-              </h2>
-              <h2 className="location-details__info-gap">
-                {location.address.number}
-              </h2>
-              <h2 className="location-details__info">
-                {location.address.city}, {location.address.province}
-              </h2>
-              <h2 className="location-details__info">
-                {location.address.postal}, {location.address.country}
+          <div className="location-details__contact-wrap">
+            <h5 className="location-details__label">Contact</h5>
+            <h2 className="location-details__info">{location.contact.name}</h2>
+            <h2 className="location-details__info-gap">
+              {location.contact.title}
+            </h2>
+            <h2 className="location-details__info">{location.contact.phone}</h2>
+            <h2 className="location-details__info">{location.contact.email}</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+  contents.push(main);
+
+  let inventoryItems = [];
+  let htmlContructor = content => {
+    let renderItems = items.items.map(item => {
+      let html = (
+        <div className="inventory__bg">
+          <div className="inventory__item" key={item.ref}>
+            <div className="inventory__product-item-wrap">
+              <div className="inventory__item-button-wrap">
+                <h5 className="inventory__label">Item</h5>
+              </div>
+              <h2 className="inventory__name">{item.name}</h2>
+              <h2 className="inventory__description">{item.description}</h2>
+            </div>
+            <div className="inventory__order-wrap">
+              <h5 className="inventory__label">Last Ordered</h5>
+              <h2 className=" inventory__date">{item.date}</h2>
+            </div>
+            <div className=" inventory__location-wrap">
+              <h5 className="inventory__label">Location</h5>
+              <h2 className=" inventory__location">
+                {item.location.city}, {item.location.country}
               </h2>
             </div>
-            <div className="location-details__contact-wrap">
-              <h5 className="location-details__label">Contact</h5>
-              <h2 className="location-details__info" key={index}>
-                {location.contact.name}
+            <div className=" inventory__quantity-wrap">
+              <h5 className="inventory__label">Quantity</h5>
+              <h2 className=" inventory__quantity">
+                {parseInt(item.quantity).toLocaleString()}
               </h2>
-              <h2 className="location-details__info-gap">
-                {location.contact.title}
-              </h2>
-              <h2 className="location-details__info">
-                {location.contact.phone}
-              </h2>
-              <h2 className="location-details__info">
-                {location.contact.email}
-              </h2>
+            </div>
+            <div className=" inventory__status-wrap">
+              <h5 className="inventory__label">Status</h5>
+              <h2 className=" inventory__status">{checkStatus(item.status)}</h2>
             </div>
           </div>
         </div>
       );
-      contents.push(main);
+      return html;
     });
-  }
+    inventoryItems.push(renderItems);
+    return renderItems;
+  };
 
-  return <section className="location-details">{contents}</section>;
+  htmlContructor(items.items);
+
+  return (
+    <div>
+      <section className="location-details">{contents}</section>
+      <section className="inventory-bg">{inventoryItems}</section>
+    </div>
+  );
 }
